1f5e3383c441ebfb9a1ae9d4f9bb9e7befc2aab5
Asm Shad (INT) <asm.shad@liz.com>
2025-01-01 12:49:58 +0600

    Add Items UI Design complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import Orders from "./pages/Orders/Orders";
+import List from "./pages/List/List";
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar></Navbar>
+      <hr />
+      <div className="app-content">
+        <Sidebar></Sidebar>
+        <Routes>
+          <Route path="/add" element={<Add></Add>} />
+          <Route path="/list" element={<List></List>} />
+          <Route path="/orders" element={<Orders></Orders>} />
+        </Routes>
+      </div>
     </div>
   );
 };
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import "./Navbar.css";
+import { assets } from "../../assets/assets";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>
+        <h1 className="logo">DelishDoor</h1>
+        <p>Admin Panel</p>
+      </div>
+      <img src={assets.profile_image} className="profile" alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
